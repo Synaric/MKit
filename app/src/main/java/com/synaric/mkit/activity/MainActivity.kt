9481b9121a0e34b429174d19bd36190fd22a3068
Synaric Yong <synaric.yong@bcc.global>
2022-06-20 17:18:32 +0800

    extract LazyLoadColumn

--- app/src/main/java/com/synaric/mkit/activity/MainActivity.kt
+++ app/src/main/java/com/synaric/mkit/activity/MainActivity.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -15,11 +13,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.paging.LoadState
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
-import androidx.paging.compose.items
 import com.synaric.art.BaseActivity
+import com.synaric.mkit.composable.LazyLoadColumn
 import com.synaric.mkit.composable.TradeRecord
 import com.synaric.mkit.data.entity.relation.TradeRecordAndGoods
 import com.synaric.mkit.theme.MKitTheme
@@ -52,48 +49,19 @@ class MainActivity : BaseActivity() {
 
     @Composable
     fun TradeRecordList(list: LazyPagingItems<TradeRecordAndGoods>) {
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxSize()
-        ) {
-            if (list.loadState.refresh == LoadState.Loading) {
-                item {
-                    Text(
-                        text = "Waiting for items to load from the backend",
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .wrapContentWidth(Alignment.CenterHorizontally)
-                    )
-                }
-            }
-
-            items(
-                items = list,
-                key = { item ->
-                    item.tradeRecord.tradeRecordId!!
-                }
-            ) { item ->
-                Text(
-                    text = "item.goods.model${item}",
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(10.dp)
-                        .wrapContentWidth(Alignment.CenterHorizontally)
-                        .wrapContentHeight(Alignment.CenterVertically),
-                    fontSize = 12.sp
-                )
-            }
-
-            if (list.loadState.append == LoadState.Loading) {
-                item {
-                    CircularProgressIndicator(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .wrapContentWidth(Alignment.CenterHorizontally)
-                    )
-                }
-            }
-
+        LazyLoadColumn(
+            list = list,
+            key = { item -> item.tradeRecord.tradeRecordId!! }
+        ) { item ->
+            Text(
+                text = "item.goods.model${item}",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp)
+                    .wrapContentWidth(Alignment.CenterHorizontally)
+                    .wrapContentHeight(Alignment.CenterVertically),
+                fontSize = 12.sp
+            )
         }
     }
 
---
+++ app/src/main/java/com/synaric/mkit/composable/LazyLoadColumn.kt
@@ -0,0 +1,56 @@
+package com.synaric.mkit.composable
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyItemScope
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.paging.LoadState
+import androidx.paging.compose.LazyPagingItems
+import androidx.paging.compose.items
+
+@Composable
+fun <T: Any> LazyLoadColumn(
+    list: LazyPagingItems<T>,
+    key: (item: T) -> Int,
+    itemContent: @Composable LazyItemScope.(value: T?) -> Unit
+) {
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        if (list.loadState.refresh == LoadState.Loading) {
+            item {
+                Text(
+                    text = "Waiting for items to load from the backend",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .wrapContentWidth(Alignment.CenterHorizontally)
+                )
+            }
+        }
+
+        items(
+            items = list,
+            key = key
+        ) { item ->
+            itemContent(item)
+        }
+
+        if (list.loadState.append == LoadState.Loading) {
+            item {
+                CircularProgressIndicator(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .wrapContentWidth(Alignment.CenterHorizontally)
+                )
+            }
+        }
+
+    }
+}
