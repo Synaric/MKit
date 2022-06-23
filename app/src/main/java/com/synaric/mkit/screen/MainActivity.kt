8e704c3a1e1f21c336a08e24d1b5d074ada7842b
Synaric Yong <synaric.yong@bcc.global>
2022-06-23 13:31:28 +0800

    test Navigation

--- app/src/main/java/com/synaric/mkit/screen/MainActivity.kt
+++ app/src/main/java/com/synaric/mkit/screen/MainActivity.kt
@@ -1,18 +1,18 @@
-package com.synaric.mkit.activity
+package com.synaric.mkit.screen
 
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.stringResource
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.synaric.art.BaseActivity
 import com.synaric.mkit.composable.LazyLoadColumn
 import com.synaric.mkit.composable.TradeRecord
@@ -20,6 +20,7 @@ import com.synaric.mkit.data.entity.relation.TradeRecordAndGoods
 import com.synaric.mkit.theme.MKitTheme
 import com.synaric.mkit.vm.MainViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 class MainActivity : BaseActivity() {
 
     private val model: MainViewModel by viewModels()
@@ -43,7 +44,11 @@ class MainActivity : BaseActivity() {
                 modifier = Modifier.fillMaxSize(),
                 color = MaterialTheme.colorScheme.background
             ) {
-                TradeRecordList(tradeRecordList)
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                ) {
+                    TradeRecordList(tradeRecordList)
+                }
             }
         }
     }
@@ -64,9 +69,9 @@ class MainActivity : BaseActivity() {
         }
     }
 
-    @Preview(showBackground = true)
-    @Composable
-    fun DefaultPreview() {
-        TradeRecord(TradeRecordAndGoods.createPreviewObject())
-    }
+//    @Preview(showBackground = true)
+//    @Composable
+//    fun DefaultPreview() {
+//        TradeRecord(TradeRecordAndGoods.createPreviewObject())
+//    }
 }
